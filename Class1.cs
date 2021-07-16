31575294f5479d42faa9a7d70e9c77b775e611f5
Drashtiben Patel <drapatel@algomau.ca>
2021-07-16 19:14:05 -0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DevOpsNuget
+{
+    public class Class1
+    {
+    }
+}
---
+++ EmplyeeInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevOpsNuget
+{
+    class EmplyeeInfo
+    {
+        public string EmployeeInformation {
+            get { return "returning a info"; }
+        }
+    }
+}
